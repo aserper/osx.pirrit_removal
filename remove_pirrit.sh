5df83caa0666939bab5f814eba13cf404bb10d1c
aserper <aserper@gmail.com>
2016-04-09 11:53:04 +0300

    Updated to also remove newer variant

--- remove_pirrit.sh
+++ remove_pirrit.sh
@@ -22,3 +22,27 @@ echo "[*] Removing malicious pf configs made by Pirrit"
 sudo rm /etc/change_net_settings.sh
 
 sudo pfctl -evf /etc/pf.conf
+
+servicePrefFileName=$(sudo defaults read /Library/Preferences/com.common.plist service_pref)
+echo “[*] Net pref file name:”
+echo $netPrefFileName
+
+appName=$(sudo defaults read /Library/Preferences/com.common.plist name)
+echo “[*] App name is:”
+echo $appName
+
+echo “[*] Removing LaunchDaemon”
+sudo launchctl unload -w "/Library/LaunchDaemons/"$servicePrefFileName
+echo [*] Killing app and osascript”
+sudo killall $appName
+sudo killall osascript
+
+echo “[*] Cleaning up…”
+sudo rm "/Library/LaunchDaemons/"$servicePrefFileName
+
+sudo rm -r "/Library/"$appName
+
+echo “[*] Removing pirrit launching script”
+sudo rm /etc/run_app.sh
+
+echo “Script finished\n”
