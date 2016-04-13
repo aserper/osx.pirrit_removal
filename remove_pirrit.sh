f4a0a79675f445e7c3c6810caabc4672a47058a6
aserper <aserper@gmail.com>
2016-04-13 11:39:20 +0300

    Update remove_pirrit.sh

--- remove_pirrit.sh
+++ remove_pirrit.sh
@@ -1,4 +1,10 @@
 echo "*** Osx.Pirit Removal script, Amit Serper, @0xamit ***"
+echo "*** BEFORE YOU RUN THIS SCRIPT, STOP! I CAN'T TAKE ANY RESPONSIBILITY ON ANY DAMAGES HAPPENING TO YOUR MACHINE. YOU ARE THE MASTER OF YOUR OWN FATE"
+echo "*** Before running this script check that you are indeed infected with osx.pirrit. TERMINATE THIS SCRIPT BY HITTING CTRL+C AND RUNNING THE FOLLOWING COMMAND:"
+echo "$ dscl . -list /Users UniqueID | grep 401"
+echo "If this command has an output then continue running this script. Else DONT"
+echo "Press any key to continue running this script, remember - I am not responsible for any unfortunate outcomes"
+read
 
 echo "[*] Getting net_pref name"
 netPrefFileName=$(sudo defaults read /Library/Preferences/com.common.plist net_pref)
